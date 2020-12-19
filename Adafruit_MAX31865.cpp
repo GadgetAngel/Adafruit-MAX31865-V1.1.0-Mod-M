8a14c5a26007ac583eba172b3359ce1576f5ae17
JoAnn Manges <joannmanges@gmail.com>
2020-12-19 10:24:32 -0500

    cleanup

--- Adafruit_MAX31865.cpp
+++ Adafruit_MAX31865.cpp
@@ -63,14 +63,15 @@
  *  On AVR, actual speed is F_CPU/2^(1 + index).
  *  On other platforms, speed should be in range given above where possible.
  */
-
-#define SPI_FULL_SPEED      0   // Set SCK to max rate
-#define SPI_HALF_SPEED      1   // Set SCK rate to half of max rate
-#define SPI_QUARTER_SPEED   2   // Set SCK rate to quarter of max rate
-#define SPI_EIGHTH_SPEED    3   // Set SCK rate to 1/8 of max rate
-#define SPI_SIXTEENTH_SPEED 4   // Set SCK rate to 1/16 of max rate
-#define SPI_SPEED_5         5   // Set SCK rate to 1/32 of max rate
-#define SPI_SPEED_6         6   // Set SCK rate to 1/64 of max rate
+#if !defined(__AVR__)
+  #define SPI_FULL_SPEED      0   // Set SCK to max rate
+  #define SPI_HALF_SPEED      1   // Set SCK rate to half of max rate
+  #define SPI_QUARTER_SPEED   2   // Set SCK rate to quarter of max rate
+  #define SPI_EIGHTH_SPEED    3   // Set SCK rate to 1/8 of max rate
+  #define SPI_SIXTEENTH_SPEED 4   // Set SCK rate to 1/16 of max rate
+  #define SPI_SPEED_5         5   // Set SCK rate to 1/32 of max rate
+  #define SPI_SPEED_6         6   // Set SCK rate to 1/64 of max rate
+#endif
 
 #ifdef __AVR__
 static SPISettings max31865_spisettings =
